f1491da165146830d388d8ee1dfb98c90b10f61b
suraj <sahusuraj7978@gmail.com>
2022-01-16 06:06:55 -0500

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,42 +1,51 @@
+import "./App.css";
+import About from "./component/About";
+import Navbar from "./component/Navbar";
+import Text from "./component/Text";
+import React, { useState } from "react";
+// import Alert from "./component/Ale/rt";
 
-import './App.css';
-// import About from './component/About';
-import Navbar from './component/Navbar';
-import Text from './component/Text';
-import React, { useState } from 'react';
-import Alert from './component/Alert';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  const [alert, setAlert] = useState(null)
-  const [mode, setMode] = useState("light")
-  const togglemode = ()=>{
-    if(mode === 'light')
-    {
-      setMode('dark');
-      document.body.style.backgroundColor = 'black'
-      showAlert('dark mode enabled','success')
+  const [alert, setAlert] = useState(null);
+  const [mode, setMode] = useState("light");
+
+  const togglemode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "black";
+      showAlert("dark mode enabled", "success");
+      document.title = "Dark mode Text utils";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("light mode enabled", "success");
+      document.title = "light mode Text utils";
     }
-    else{
-      setMode('light');
-      document.body.style.backgroundColor = 'white'
-      showAlert('light mode enabled','success')
-    }
-  }
-  const showAlert = (msg,type)=>{
+  };
+  const showAlert = (msg, type) => {
     setAlert({
-      msg:msg,
-      type:type
-    })
-    setTimeout(()=>{
-      setAlert(null)
-    },1500)
-  }
+      msg: msg,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
   return (
     <>
-    <Navbar Mode = {mode} togglemode ={togglemode} />
-    <Alert alert = {alert}/>
-    <Text Mode = {mode} togglemode ={togglemode} showAlert={showAlert}/>
-    {/* <About/> */}
+    <Router>
+    <Navbar Mode={mode} togglemode={togglemode} />
+    <Switch>
+      <Route path="/about">
+      <About/>
+      </Route>
+      <Route path="/">
+      <Text Mode={mode}/>
+      </Route>
+      </Switch>
+      </Router>
     </>
   );
 }
